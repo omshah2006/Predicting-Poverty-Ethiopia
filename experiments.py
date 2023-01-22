48a0e208eb245ba68c8392032bad9f5508aa6bfe
Om Shah <omshah2006@gmail.com>
2023-01-22 13:02:31 -0800

    new experiment

--- experiments.py
+++ experiments.py
@@ -65,7 +65,7 @@ def run_cifar_sample_vgg():
         platform="cloud",
         strategy="tpu",
         model_name="sample_vgg",
-        dataset="cifar",
+        dataset="imagery",
         optimizer="adam",
         lr_rate=1e-3,
         momentum=0.9,
