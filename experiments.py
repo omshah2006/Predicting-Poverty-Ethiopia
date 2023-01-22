3d21365c2cafe4bd2d1ffcaf1f5145bccede65bc
Om Shah <omshah2006@gmail.com>
2023-01-21 20:54:29 -0800

    tweaked params

--- experiments.py
+++ experiments.py
@@ -27,6 +27,34 @@ def run_cifar_vgg16():
         verbose=2,
     )
 
+
+def run_cifar_vgg16_bn():
+    trained_model = train_model(
+        experiment_name='cifar_vgg16',
+        platform="cloud",
+        strategy="tpu",
+        model_name="vgg16_bn",
+        dataset="cifar",
+        optimizer="adam",
+        lr_rate=0.00001,
+        num_classes=10,
+        weights="imagenet",
+        use_custom_top=True,
+        input_shape=(32, 32, 3),
+        batch_size=512,
+        use_l2_regularizer=True,
+        batch_norm_decay=0.9,
+        batch_norm_epsilon=1e-5,
+        loss_func="categorical_crossentropy",
+        metrics=["accuracy"],
+        steps_per_execution=32,
+        num_epochs=100,
+        train_steps=int(50000 / 512),
+        val_steps=10000,
+        verbose=2,
+    )
+
+
 def run_cifar_sample_cnn():
     trained_model = train_model(
         experiment_name='cifar_sample_cnn',
@@ -81,4 +109,4 @@ def run_local():
 
 
 if __name__ == '__main__':
-    run_cifar_vgg16()
+    run_cifar_vgg16_bn()
