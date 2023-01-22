66bea9d28f378b3d66dcee0f9907607bb912802c
Om Shah <omshah2006@gmail.com>
2023-01-21 23:30:03 -0800

    new experiment

--- test.py
+++ test.py
@@ -1,136 +1,172 @@
-import time
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras.utils import np_utils
-from keras.datasets import cifar10
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import BatchNormalization
-from keras.callbacks import ModelCheckpoint, CSVLogger
-from keras.callbacks import LearningRateScheduler as lr_scheduler
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def summary(history):
-    # plot loss
-    plt.subplot(211)
-    plt.title('Cross Entropy Loss')
-    plt.plot(history.history['loss'], color='blue', label='train')
-    plt.plot(history.history['val_loss'], color='orange', label='test')
-    plt.legend(loc=0)
-    # plot accuracy
-    plt.subplot(212)
-    plt.title('Classification Accuracy')
-    plt.plot(history.history['accuracy'], color='blue', label='train')
-    plt.plot(history.history['val_accuracy'], color='orange', label='test')
-    plt.legend(loc=0)
-    # save plot to file
-    plt.subplots_adjust(wspace=None, hspace=1.0)
-    filename = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-    plt.savefig(filename + ".png")
-    plt.close()
+def normalization(train_images, test_images):
+    mean = np.mean(train_images, axis=(0, 1, 2, 3))
+    std = np.std(train_images, axis=(0, 1, 2, 3))
+    train_images = (train_images - mean) / (std + 1e-7)
+    test_images = (test_images - mean) / (std + 1e-7)
+    return train_images, test_images
 
 
-def lr_decay(epoch):
-    lr = 0.001
-    if epoch > 75:
-        lr = 0.0005
-    if epoch > 100:
-        lr = 0.0003
-    if epoch > 128:
-        lr = 0.0001
-    return lr
+def load_images():
+    (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+    train_images = train_images.astype(np.float32)
+    test_images = test_images.astype(np.float32)
+
+    (train_images, test_images) = normalization(train_images, test_images)
+
+    train_labels = to_categorical(train_labels, 10)
+    test_labels = to_categorical(test_labels, 10)
+
+    # train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(
+    #     buffer_size=10000).batch(batch_size)
+    # test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
+
+    return train_images, train_labels, test_images, test_labels
 
 
-def mdlTrain(train_feature, train_label, test_feature, test_label):
-    # model definition
-    model = Sequential()
+class ConvBNRelu(tf.keras.Model):
+    def __init__(self, filters, kernel_size=3, strides=1, padding='SAME', weight_decay=0.0005, rate=0.4, drop=True):
+        super(ConvBNRelu, self).__init__()
+        self.drop = drop
+        self.conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides,
+                                        padding=padding, kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self.batchnorm = tf.keras.layers.BatchNormalization()
+        self.dropOut = keras.layers.Dropout(rate=rate)
 
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform',
-                     input_shape=(224, 224, 3)))
-    model.add(BatchNormalization())
-    model.add(
-        Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D())
-    model.add(Dropout(0.2))
+    def call(self, inputs, training=False):
+        layer = self.conv(inputs)
+        layer = tf.nn.relu(layer)
+        layer = self.batchnorm(layer)
+        if self.drop:
+            layer = self.dropOut(layer)
 
-    model.add(
-        Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(
-        Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D())
-    model.add(Dropout(0.3))
-
-    model.add(
-        Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(
-        Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling2D())
-    model.add(Dropout(0.4))
-
-    model.add(Flatten())
-    model.add(Dense(units=128, kernel_initializer='he_uniform', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(units=10, kernel_initializer='he_uniform', activation='softmax'))
-
-    print(model.summary())
-
-    # training definition
-    batch_num = 64
-    epoch_num = 200
-    opt = Adam()
-    # datagen = ImageDataGenerator(rotation_range=15, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-    # datagen.fit(train_feature)
-    # checkpoint = ModelCheckpoint("cifar10_best.h5", monitor='val_accuracy', verbose=0, save_best_only=True, mode='max')
-    # csv_logger = CSVLogger('training.csv')
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-    history = model.fit(x=train_feature, y=train_label, batch_size=batch_num, \
-                                  steps_per_epoch=int(len(train_feature) / batch_num), epochs=epoch_num,
-                                  validation_data=(test_feature, test_label), \
-                                  verbose=2)
-    summary(history)
-
-    # accuracy evaluation
-    accuracy = model.evaluate(test_feature, test_label)
-    print('\n[Accuracy] = ', accuracy[1])
-
-    return model
+        return layer
 
 
-# ---------------------------------------------------------------------------
+class VGG16Model(tf.keras.Model):
+    def __init__(self):
+        super(VGG16Model, self).__init__()
+        self.conv1 = ConvBNRelu(filters=64, kernel_size=[3, 3], rate=0.3)
+        self.conv2 = ConvBNRelu(filters=64, kernel_size=[3, 3], drop=False)
+        self.maxPooling1 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+        self.conv3 = ConvBNRelu(filters=128, kernel_size=[3, 3])
+        self.conv4 = ConvBNRelu(filters=128, kernel_size=[3, 3], drop=False)
+        self.maxPooling2 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+        self.conv5 = ConvBNRelu(filters=256, kernel_size=[3, 3])
+        self.conv6 = ConvBNRelu(filters=256, kernel_size=[3, 3])
+        self.conv7 = ConvBNRelu(filters=256, kernel_size=[3, 3], drop=False)
+        self.maxPooling3 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+        self.conv11 = ConvBNRelu(filters=512, kernel_size=[3, 3])
+        self.conv12 = ConvBNRelu(filters=512, kernel_size=[3, 3])
+        self.conv13 = ConvBNRelu(filters=512, kernel_size=[3, 3], drop=False)
+        self.maxPooling5 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+        self.conv14 = ConvBNRelu(filters=512, kernel_size=[3, 3])
+        self.conv15 = ConvBNRelu(filters=512, kernel_size=[3, 3])
+        self.conv16 = ConvBNRelu(filters=512, kernel_size=[3, 3], drop=False)
+        self.maxPooling6 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+        self.flat = keras.layers.Flatten()
+        self.dropOut = keras.layers.Dropout(rate=0.5)
+        self.dense1 = keras.layers.Dense(units=512,
+                                         activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.0005))
+        self.batchnorm = tf.keras.layers.BatchNormalization()
+        self.dense2 = keras.layers.Dense(units=10)
+        self.softmax = keras.layers.Activation('softmax')
 
-# load cifar10 data
-(train_feature, train_label), (test_feature, test_label) = cifar10.load_data()
+    def call(self, inputs, training=False):
+        net = self.conv1(inputs)
+        net = self.conv2(net)
+        net = self.maxPooling1(net)
+        net = self.conv3(net)
+        net = self.conv4(net)
+        net = self.maxPooling2(net)
+        net = self.conv5(net)
+        net = self.conv6(net)
+        net = self.conv7(net)
+        net = self.maxPooling3(net)
+        net = self.conv11(net)
+        net = self.conv12(net)
+        net = self.conv13(net)
+        net = self.maxPooling5(net)
+        net = self.conv14(net)
+        net = self.conv15(net)
+        net = self.conv16(net)
+        net = self.maxPooling6(net)
+        net = self.dropOut(net)
+        net = self.flat(net)
+        net = self.dense1(net)
+        net = self.batchnorm(net)
+        net = self.dropOut(net)
+        net = self.dense2(net)
+        net = self.softmax(net)
+        return net
 
-# data preprocessing
-# reshape
-train_feature_vector = np.resize(train_feature, (50000, 224, 224, 3))
-test_feature_vector = np.resize(test_feature, (10000, 224, 224, 3))
 
-# feature normalization
-# z-score
-mean = np.mean(train_feature_vector, axis=(0, 1, 2, 3))
-std = np.std(train_feature_vector, axis=(0, 1, 2, 3))
-train_feature_normal = (train_feature_vector - mean) / (std + 1e-7)
-test_feature_normal = (test_feature_vector - mean) / (std + 1e-7)
+if __name__ == '__main__':
+    print(tf.__version__)
+    print(keras.__version__)
+    #
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # if gpus:
+    #     # 텐서플로가 첫 번째 GPU만 사용하도록 제한
+    #     try:
+    #         print('start with GPU 7')
+    #         tf.config.experimental.set_visible_devices(gpus[7], 'GPU')
+    #         tf.config.experimental.set_memory_growth(gpus[7], True)
+    #     except RuntimeError as e:
+    #         # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
+    #         print(e)
 
-# one-hot encoding
-train_label_onehot = np_utils.to_categorical(train_label)
-test_label_onehot = np_utils.to_categorical(test_label)
+    training_epochs = 250
+    batch_size = 128
+    learning_rate = 0.1
+    momentum = 0.9
+    lr_decay = 1e-6
+    lr_drop = 20
 
-# train model
-model = mdlTrain(train_feature_normal, train_label_onehot, test_feature_normal, test_label_onehot)
-accuracy = model.evaluate(test_feature_normal, test_label_onehot)
-print('\n[Accuracy] = ', accuracy)
+    tf.random.set_seed(777)
 
-# save model
-filename = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-model.save(filename + ".h5")
-del model
+    def lr_scheduler(epoch):
+        return learning_rate * (0.5 ** (epoch // lr_drop))
+
+    reduce_lr = keras.callbacks.LearningRateScheduler(lr_scheduler)
+
+    train_images, train_labels, test_images, test_labels = load_images()
+
+    # data augmentation
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(train_images)
+
+    model = VGG16Model()
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate,
+                                        decay=1e-6, momentum=momentum, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    model.fit_generator(datagen.flow(train_images, train_labels,
+                                     batch_size=batch_size), epochs=training_epochs, verbose=2, callbacks=[reduce_lr],
+                        steps_per_epoch=train_images.shape[0] // batch_size, validation_data=(test_images, test_labels))
+
+    model.save_weights('cifar10vgg_custom.h5')
