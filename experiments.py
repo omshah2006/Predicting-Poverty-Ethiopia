f472a5290eb677713e2d1714052384c698df2000
Om Shah <omshah2006@gmail.com>
2023-01-22 20:22:10 -0800

    new experiment

--- experiments.py
+++ experiments.py
@@ -24,7 +24,7 @@ def run_cifar_vgg16():
         loss_func="categorical_crossentropy",
         metrics=["accuracy"],
         steps_per_execution=32,
-        num_epochs=100,
+        num_epochs=10,
         train_steps=int(50000 / 128),
         val_steps=10000,
         verbose=2,
