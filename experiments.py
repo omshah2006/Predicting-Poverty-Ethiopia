4bf36fdfc3de9964b1aa469f0d1cbdc685b998f1
Om Shah <omshah2006@gmail.com>
2023-01-24 17:28:10 -0800

    .

--- experiments.py
+++ experiments.py
@@ -37,7 +37,7 @@ def run_imagery_vgg16():
         strategy="tpu",
         model_name="sample_vgg",
         dataset="imagery",
-        optimizer="sgd",
+        optimizer="adam",
         lr_rate=1e-3,
         momentum=0.9,
         weight_decay=1e-4,
