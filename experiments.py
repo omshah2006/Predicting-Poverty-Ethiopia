2640e881d439fbfd9b4f1d78cb52c58d72cc2ad6
Om Shah <omshah2006@gmail.com>
2023-01-27 16:01:53 -0800

    .

--- experiments.py
+++ experiments.py
@@ -30,6 +30,35 @@ def run_cifar_vgg16():
         verbose=2,
     )
 
+def run_cifar_vgg16_regression():
+    trained_model = train_model(
+        experiment_name='cifar_sample_vgg_10_epoch_224_224_run',
+        platform="cloud",
+        strategy="tpu",
+        model_name="sample_vgg",
+        dataset="cifar",
+        optimizer="adam",
+        lr_rate=1e-3,
+        momentum=0.9,
+        weight_decay=1e-4,
+        num_classes=1,
+        weights=None,
+        use_custom_top=True,
+        input_shape=(224, 224, 3),
+        fl_activation="linear",
+        batch_size=128,
+        use_l2_regularizer=True,
+        batch_norm_decay=0.9,
+        batch_norm_epsilon=1e-5,
+        loss_func="MeanSquaredError",
+        metrics=["RootMeanSquaredError"],
+        steps_per_execution=32,
+        num_epochs=2,
+        train_steps=int(50000 / 128),
+        val_steps=10000,
+        verbose=2,
+    )
+
 def run_imagery_vgg16():
     trained_model = train_model(
         experiment_name='imagery_sample_vgg_100_epoch_run',
@@ -90,4 +119,4 @@ def run_local():
 
 
 if __name__ == '__main__':
-    run_cifar_vgg16()
+    run_cifar_vgg16_regression()
