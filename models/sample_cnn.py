5603f90c45ffc8bd3417dcf847e1325da2985374
Om Shah <omshah2006@gmail.com>
2023-01-29 16:45:47 -0800

    new experiment

--- models/sample_cnn.py
+++ models/sample_cnn.py
@@ -8,27 +8,27 @@ def sample_cnn(num_classes, input_shape, fl_activation):
 
     model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', input_shape=input_shape))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D())
     model.add(layers.Dropout(0.2))
 
-    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D())
     model.add(layers.Dropout(0.4))
 
     model.add(layers.Flatten())
-    model.add(layers.Dense(units=128, kernel_initializer='he_uniform', activation='elu'))
+    model.add(layers.Dense(units=128, kernel_initializer='he_uniform', activation='gelu'))
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(units=num_classes, kernel_initializer='he_uniform', activation=fl_activation))
 
