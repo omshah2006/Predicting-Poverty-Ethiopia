cea6816a86bffa3797d9411f9f2e5ad431a29756
Om Shah <omshah2006@gmail.com>
2023-02-03 09:27:18 -0800

    new experiment

--- models/sample_cnn.py
+++ models/sample_cnn.py
@@ -13,9 +13,10 @@ def sample_cnn(num_classes, input_shape, fl_activation):
     # model.add(layers.MaxPooling2D())
     # model.add(layers.Dropout(0.2))
     model.add(layers.Conv2D(64, (3, 3), activation='gelu', padding='same', input_shape=input_shape))
-    model.add(layers.Conv2D(64, (3, 3), activation='gelu', padding='same'))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(64, (3, 3), activation='gelu', padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(layers.Dropout(0.25))
 
     model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', kernel_initializer='he_uniform', activation='gelu'))
