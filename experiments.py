f0ff3371e02e7c60d00182663abfb166a4501fe9
Om Shah <omshah2006@gmail.com>
2023-02-22 16:24:03 -0800

    new experiment

--- experiments.py
+++ experiments.py
@@ -88,7 +88,7 @@ def run_imagery_vgg16():
         loss_func="MeanSquaredError",
         metrics=["RootMeanSquaredError"],
         steps_per_execution=32,
-        num_epochs=50,
+        num_epochs=49,
         train_steps=int(4559 / 128),
         val_steps=1302,
         verbose=2,
